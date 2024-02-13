45c9cb2401c8dd5a508d58fc97e6a24ac904e589
Eugene <eugenemz0908@gmail.com>
2024-02-13 13:32:16 +0200

    DataServices_Connect

---
+++ StudentAppMint/Data/DataService.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using System.Data;
+
+namespace StudentAppMint.Data
+{
+    public class DataService
+    {
+        public void ExecWithoutReturn(string procedureName, object? param)
+        {
+            using (var connection = StartConfig._dbContext.CreateConnection())
+            {
+                connection.Open();
+                connection.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+                connection.Close();
+            }
+        }
+        public T ExecReturnScalar<T>(string procedureName, object? procedureParams)
+        {
+            using (var connection = StartConfig._dbContext.CreateConnection())
+            {
+                connection.Open();
+                var result = connection.Query<T>(sql: procedureName,
+                    param: procedureParams,
+                    commandType: CommandType.StoredProcedure).FirstOrDefault();
+                connection.Close();
+                return result;
+            }
+        }
+        public IEnumerable<T> ExecReturnList<T>(string procedureName, object? param)
+        {
+            using (var connection = StartConfig._dbContext.CreateConnection())
+            {
+                connection.Open();
+                connection.Close();
+                return connection.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+
+            }
+        }
+    }
+}
---
+++ StudentAppMint/Data/StartConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentAppMint.Data
+{
+    public class StartConfig
+    {
+        public static IConfiguration config;
+        public static DataBaseConnect _dbContext;
+        public static void InitConfig()
+        {
+            var builder = new ConfigurationBuilder();
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            config = builder.Build();
+            _dbContext = new DataBaseConnect(config);
+        }
+    }
+}
